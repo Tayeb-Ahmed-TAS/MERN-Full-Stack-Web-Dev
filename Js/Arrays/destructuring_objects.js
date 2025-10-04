df51f52b3de9418bb6593e894d1ce4a6ad08bb7f
Tayeb Ahmed <104755858+Tayeb-Ahmed-TAS@users.noreply.github.com>
2025-10-04 12:21:54 +0600

    Array Methods

---
+++ Js/Arrays/destructuring_objects.js
@@ -0,0 +1,10 @@
+const students={
+name:"Tony",
+age:14,
+class:9,
+subjects:["English","Math","Science","Higher Math"],
+username:"example@example.com",
+password:"@abcd1234",
+};
+
+let {username, password}=students;
